ae13b1f40da310c59b4bd45ebec4b3cbdeefb90e
John Bagley <jdbagley@gmail.com>
2014-08-22 11:45:26 -0400

    Add Shouts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
+  before_filter :check_for_logged_in
   def show
+  end
+
+  private
+
+  def check_for_logged_in
+    if signed_in?
+      redirect_to dashboard_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#show'
+  resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :shouts, only: [:create]
 end
