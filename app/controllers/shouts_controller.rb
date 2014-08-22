a2525116ff4e32b8d07c1edd34c9e190a8b4057e
John Bagley <jdbagley@gmail.com>
2014-08-22 13:10:06 -0400

    Add validation and alert

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -5,8 +5,12 @@ class ShoutsController < ApplicationController
 
   def create
     shout = current_user.shouts.build(shout_params)
-    shout.save
-    redirect_to dashboard_path
+    if shout.save
+      redirect_to dashboard_path
+    else 
+      flash.alert = "Could not shout"
+      redirect_to dashboard_path
+    end
   end
 
   def shout_params
